0eb4d79982c29253b0711ea1acf91ff7d876f344
Xiang Liu <lx1036@126.com>
2019-07-08 19:50:02 +0800

    Export VuePress site to pdf

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  title: 'docs/pdf/刘祥的简历',
+  plugins: ['vuepress-plugin-export'],
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'lx1036',
+      description: 'lx1036 的简历'
+    }
+  },
+  themeConfig: {
+    docsRepo: 'lx1036/lx1036.github.io',
+    docsDir: 'docs',
+    docsBranch: 'docs',
+    editLinks: false,
+    repo: 'lx1036/lx1036.github.io',
+    locales: {
+      '/': {
+        sidebar: [
+          {
+            title: '关于我',
+            collapsable: false,
+            children: [
+              ['/专业技能', '专业技能'],
+              ['/工作经历', '工作经历'],
+              ['/项目经历', '项目经历'],
+              ['/教育经历', '教育经历'],
+              ['/自我介绍', '自我介绍'],
+            ]
+          },
+        ],
+      },
+    },
+  },
+}
