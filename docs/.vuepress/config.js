770aa0355e4f28787afcb024dbd4b03fe0f5936d
Xiang Liu <lx1036@126.com>
2019-07-17 15:50:56 +0800

    置顶教育经历，简化技能清单

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'docs/pdf/北航硕士刘祥的简历',
-  exportPdfOrder: ["README.md", "专业技能.md", "工作经历.md", "项目经历.md", "教育经历.md", "自我介绍.md"],
+  exportPdfOrder: ["README.md", "教育经历.md", "专业技能.md", "工作经历.md", "项目经历.md", "自我介绍.md"],
   head: [
     ['link', {rel: 'icon', href: '/logo.png'}]
   ],
@@ -25,10 +25,10 @@ module.exports = {
             title: '关于我',
             collapsable: false,
             children: [
+              ['/教育经历', '教育经历'],
               ['/专业技能', '专业技能'],
               ['/工作经历', '工作经历'],
               ['/项目经历', '项目经历'],
-              ['/教育经历', '教育经历'],
               ['/自我介绍', '自我介绍'],
             ]
           },
